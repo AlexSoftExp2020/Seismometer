8ac254b07ea394ee02c07557df60199cc867fef2
Alex <oleksii.pidhornyi@gmail.com>
2025-01-25 20:56:48 +0100

    Initial Commit

---
+++ Seismometer/SeismometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeismometerApp.swift
+//  Seismometer
+//
+//  Created by Oleksii on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SeismometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
