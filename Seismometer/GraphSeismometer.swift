dce33559eeb3085335fb48a5134e70a83e66b523
Alex <oleksii.pidhornyi@gmail.com>
2025-01-29 22:02:58 +0100

    add graph seismometer

---
+++ Seismometer/GraphSeismometer.swift
@@ -0,0 +1,18 @@
+//
+//  GraphSeismometer.swift
+//  Seismometer
+//
+//  Created by Oleksii on 29.01.2025.
+//
+
+import SwiftUI
+
+struct GraphSeismometer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GraphSeismometer()
+}
