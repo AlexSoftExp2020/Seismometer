03388a2213f0b77eeb104cc0f0a77e7f77b22aa8
Alex <oleksii.pidhornyi@gmail.com>
2025-01-30 20:44:37 +0100

    extend graph seismometer

--- Seismometer/GraphSeismometer.swift
+++ Seismometer/GraphSeismometer.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct GraphSeismometer: View {
-    //MARK: TODO @EnvironmentObject private var detector: MotionDetector
+    @EnvironmentObject private var detector: MotionDetector
+    @State private var data = [Double]()
+let maxData =  1000
+    
+    @State private var sensitivity = 0.0
+    let graphMaxValueMostSensitive = 0.01
+    let graphMaxValueLeastSensitive = 1.0
+    
+    var graphMaxValue: Double {
+        graphMaxValueMostSensitive + (1 - sensitivity) * (graphMaxValueLeastSensitive - graphMaxValueMostSensitive)
+    }
+    
+    var graphMinValue: Double {
+        -graphMaxValue
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            //MARK: TODO LineGraph
+        }
     }
 }
 
