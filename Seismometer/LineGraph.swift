a73f94d2ad4e347962eeb9623da032840c7bf50f
Alex <oleksii.pidhornyi@gmail.com>
2025-01-30 20:52:10 +0100

    add line graph

---
+++ Seismometer/LineGraph.swift
@@ -0,0 +1,31 @@
+//
+//  LineGraph.swift
+//  Seismometer
+//
+//  Created by Oleksii on 30.01.2025.
+//
+
+import SwiftUI
+
+struct LineGraph: View {
+    let data: [Double]
+    let maxData: Int
+    
+    let minValue: Double
+    let maxValue: Double
+    
+    let gridSpacing = 250
+    
+    @State private var timestep = 0
+    
+//    func yGraphPosition(_ dataItem: Double, in size: CGSize) -> Double {
+//
+//    }
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineGraph()
+}
