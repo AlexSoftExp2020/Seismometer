ea7bf9bb4868422c9f2f18a2a85641356e215981
Alex <oleksii.pidhornyi@gmail.com>
2025-01-30 20:31:17 +0100

    add extension uidevice orientation

--- Seismometer/MotionDetector.swift
+++ Seismometer/MotionDetector.swift
@@ -80,3 +80,22 @@ extension MotionDetector {
         return self
     }
 }
+
+extension UIDeviceOrientation {
+    func adjustedRollAndPitch(_ attitude: CMAttitude) -> (roll: Double, pitch: Double) {
+        switch self {
+        case .unknown, .faceUp, .faceDown:
+            return (attitude.roll, -attitude.pitch)
+        case .landscapeLeft:
+            return (attitude.pitch, -attitude.roll)
+        case .portrait:
+            return (attitude.roll, attitude.pitch)
+        case .portraitUpsideDown:
+            return (-attitude.roll, -attitude.pitch)
+        case .landscapeRight:
+            return (-attitude.pitch, attitude.roll)
+        @unknown default:
+            return (attitude.roll, attitude.pitch)
+        }
+    }
+}
