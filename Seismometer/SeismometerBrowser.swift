220e4a52d83c15e9b14ece4deda8defa2aca73b9
Alex <oleksii.pidhornyi@gmail.com>
2025-01-31 19:09:51 +0100

    add seismometer browser

---
+++ Seismometer/SeismometerBrowser.swift
@@ -0,0 +1,18 @@
+//
+//  SeismometerBrowser.swift
+//  Seismometer
+//
+//  Created by Oleksii on 31.01.2025.
+//
+
+import SwiftUI
+
+struct SeismometerBrowser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeismometerBrowser()
+}
