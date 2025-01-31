80608cd310181cf7927c68ad21c71227c490a013
Alex <oleksii.pidhornyi@gmail.com>
2025-01-31 19:13:26 +0100

    add property to seismometer browser

--- Seismometer/SeismometerBrowser.swift
+++ Seismometer/SeismometerBrowser.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SeismometerBrowser: View {
+    @StateObject private var detector = MotionDetector(updateInterval: 0.01)
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
