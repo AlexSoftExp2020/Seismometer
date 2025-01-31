60b5dcf58cf9f19fbbee53525085b2fb7c6af710
Alex <oleksii.pidhornyi@gmail.com>
2025-01-31 18:57:31 +0100

    add properties to needle seismometer

--- Seismometer/NeedleSeismometer.swift
+++ Seismometer/NeedleSeismometer.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct NeedleSeismometer: View {
+    @EnvironmentObject var motionDetector: MotionDetector
+    
+    let needleAnchor = UnitPoint(x: 0.5, y: 1)
+    let amplification = 2.0
+    var rotationAngle: Angle {
+        Angle(radians: -motionDetector.zAcceleration * amplification)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
